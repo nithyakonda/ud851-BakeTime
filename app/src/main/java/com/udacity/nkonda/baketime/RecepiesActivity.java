84975ab32863b36a2581c4160c29d5f66e463bcd
Nkonda <nkonda@roamdata.com>
2018-03-25 23:02:48 -0400

    Initial commit

---
+++ app/src/main/java/com/udacity/nkonda/baketime/RecepiesActivity.java
@@ -0,0 +1,13 @@
+package com.udacity.nkonda.baketime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecepiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recepies);
+    }
+}
